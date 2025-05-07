5e622f669509a96a16240e064a95290799bba240
yeyz <yeyouzhi@taqu.cn>
2025-05-07 22:59:47 +0800

    header

--- app/roth-ira-calculator/layout.tsx
+++ app/roth-ira-calculator/layout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-white">
+      <Header />
       {children}
     </div>
   );
